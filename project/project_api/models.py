85ab9e13bdb9033bfa50b574a623e952333c15fc
Ajay1219 <ajaychoudhary19990@gmail.com>
2020-09-19 18:03:57 +0530

    Solved admin issue

--- project/project_api/models.py
+++ project/project_api/models.py
@@ -25,9 +25,9 @@ class UserProfileManager(BaseUserManager):
 
     return user
 
-  def create_superuser(self, email, name, password):
+  def create_superuser(self, email, phone, name, password):
     """Creates and saves a new superuser with givent details."""
-    user = self.create_user(email, name, password)
+    user = self.create_user(email, phone, name, password)
 
     user.is_superuser = True
     user.is_staff = True
